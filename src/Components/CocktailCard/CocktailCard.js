92d6c5254375ff00ddf96166152e84ee895c47c2
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-07-10 18:15:16 +0200

    Update

--- src/Components/CocktailCard/CocktailCard.js
+++ src/Components/CocktailCard/CocktailCard.js
@@ -1,34 +1,11 @@
-import React, { useContext } from "react";
+import React /*useContext*/ from "react";
 import "./CocktailCard.scss";
-import { CocktailContext } from "../../App";
-import { Cocktail } from "../../model/Cocktail";
+//import { CocktailContext } from "../../App";
+//import { Cocktail } from "../../model/Cocktail";
 
 export function CocktailCard(props) {
-  const [cocktails, setCocktails] = useContext(CocktailContext);
-  console.log(cocktails);
-
-  const handleClick = () => {
-    setCocktails([
-      new Cocktail("mojito", require("../../assets/cocktails/mojito.png"), [
-        "6 cl rhum",
-        "3 cl jus de citron vert",
-        "7 feuilles de menthe",
-        "2 cl sirop sucre de cannes",
-        "eau gazeuse"
-      ]),
-      new Cocktail(
-        "gin fizz",
-        require("../../assets/cocktails/ginn_fizz.png"),
-        [
-          "6 cl gin",
-          "4 cl jus de citron",
-          "7 feuilles de menthe",
-          "2 cl sirop sucre canne",
-          "12 cl eau gazeuse"
-        ]
-      )
-    ]);
-  };
+  //const [cocktails, setCocktails] = useContext(CocktailContext);
+  //console.log(cocktails);
 
   return (
     <div className="cocktailCard">
@@ -40,7 +17,6 @@ export function CocktailCard(props) {
           <li>{ingredient}</li>
         ))}
       </ol>
-      <button onClick={handleClick}>clic me</button>
     </div>
   );
 }
